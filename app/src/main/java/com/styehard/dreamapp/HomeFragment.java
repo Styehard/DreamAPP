d42f298c6184db10e5c5088b398aea3cf34bb690
Maksymilian Jan Szymanski <maksymilian.szymanski@alumnos.upm.es>
2018-08-25 12:38:54 +0200

    Initial commit

---
+++ app/src/main/java/com/styehard/dreamapp/HomeFragment.java
@@ -0,0 +1,132 @@
+package com.styehard.dreamapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.styehard.dreamapp.Models.DataModel;
+import com.styehard.dreamapp.Models.MyData;
+
+import java.util.ArrayList;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class HomeFragment extends Fragment {
+
+    private static RecyclerView.Adapter adapter;
+    private GridLayoutManager layoutManager;
+    private static RecyclerView recyclerView;
+    private static ArrayList<DataModel> data;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Context context = getContext();
+        //just change the fragment_dashboard
+        //with the fragment you want to inflate
+        //like if the class is HomeFragment it should have R.layout.home_fragment
+        //if it is DashboardFragment it should have R.layout.fragment_dashboard
+        View view = inflater.inflate(R.layout.fragment_home, null);
+//        myOnClickListener = new MyOnClickListener(context);
+
+        recyclerView = view.findViewById(R.id.my_recycler_view);
+        //recyclerView.setHasFixedSize(true);
+
+        layoutManager = new GridLayoutManager(context,2);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+        data = new ArrayList<DataModel>();
+        for (int i = 0; i < MyData.nameArray.length; i++) {
+            data.add(new DataModel(
+                    MyData.nameArray[i]
+            ));
+        }
+
+        adapter = new CustomAdapter(data);
+        recyclerView.setAdapter(adapter);
+
+        return view;
+    }
+
+
+
+
+
+//    private static class MyOnClickListener implements View.OnClickListener {
+//
+//        private final Context context;
+//
+//        private MyOnClickListener(Context context) {
+//            this.context = context;
+//        }
+//
+//        @Override
+//        public void onClick(View v) {
+//            removeItem(v);
+//        }
+//
+//        private void removeItem(View v) {
+//            int selectedItemPosition = recyclerView.getChildPosition(v);
+//            RecyclerView.ViewHolder viewHolder
+//                    = recyclerView.findViewHolderForPosition(selectedItemPosition);
+//            TextView textViewName
+//                    = (TextView) viewHolder.itemView.findViewById(R.id.textViewName);
+//            String selectedName = (String) textViewName.getText();
+//            int selectedItemId = -1;
+//            for (int i = 0; i < MyData.nameArray.length; i++) {
+//                if (selectedName.equals(MyData.nameArray[i])) {
+//                    selectedItemId = MyData.id_[i];
+//                }
+//            }
+//            removedItems.add(selectedItemId);
+//            data.remove(selectedItemPosition);
+//            adapter.notifyItemRemoved(selectedItemPosition);
+//        }
+//    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        super.onCreateOptionsMenu(menu);
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        super.onOptionsItemSelected(item);
+//        if (item.getItemId() == R.id.add_item) {
+//            //check if any items to add
+//            if (removedItems.size() != 0) {
+//                addRemovedItemToList();
+//            } else {
+//                Toast.makeText(this, "Nothing to add", Toast.LENGTH_SHORT).show();
+//            }
+//        }
+//        return true;
+//    }
+//
+//    private void addRemovedItemToList() {
+//        int addItemAtListPosition = 3;
+//        data.add(addItemAtListPosition, new DataModel(
+//                MyData.nameArray[removedItems.get(0)],
+//                MyData.versionArray[removedItems.get(0)],
+//                MyData.id_[removedItems.get(0)],
+//                MyData.drawableArray[removedItems.get(0)]
+//        ));
+//        adapter.notifyItemInserted(addItemAtListPosition);
+//        removedItems.remove(0);
+}
+
+
---
+++ app/src/main/java/com/styehard/dreamapp/MainActivity.java
@@ -0,0 +1,71 @@
+package com.styehard.dreamapp;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment fragment = null;
+
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    fragment = new HomeFragment();
+                    break;
+
+                case R.id.navigation_social:
+                    fragment = new SocialFragment();
+                    break;
+
+                case R.id.navigation_profile:
+                    fragment = new ProfileFragment();
+                    break;
+            }
+
+            return loadFragment(fragment);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Set deafault fragment
+        loadFragment(new HomeFragment());
+
+        //Set listener
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+
+        navigation.setSelectedItemId(R.id.navigation_home);
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+}
